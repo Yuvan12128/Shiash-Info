e0f0ba1973ff7d2951ddeac334b519df8a338e73
Yuvan12128 <yuvan12128@gmail.com>
2024-03-03 16:26:37 +0530

    'update'

---
+++ Java Program/demo.java
@@ -0,0 +1,19 @@
+class demo {
+	
+int b=200;
+public static void main(String []args){
+	int a =100;
+	System.out.println("my first program");
+		System.out.println(a);
+		demo d=new demo();
+		System.out.println(d.b);
+		System.out.println(d.b1);
+}
+int b1=2000;
+}
+/*D:\shiash>java demo.java
+my first program
+100
+200
+2000
+*/
