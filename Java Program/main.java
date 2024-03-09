12f361a7737e23c48ff97d60ad74ab1399e3aec4
Yuvan12128 <yuvan12128@gmail.com>
2024-03-09 13:57:22 +0530

    update_my_code'

---
+++ Java Program/main.java
@@ -0,0 +1,71 @@
+package demo;
+
+public class main {
+
+	public static void main(String... args) {
+		// TODO Auto-generated method stub
+		I();
+		II();
+		III();
+		IV();
+		V();
+		VI();
+		VII();
+		VIII();
+	}
+	private static void VIII() {
+		System.out.println("check for correctness");
+		//out:check for correctness
+	}
+	private static void VII() {
+		int $=10;
+		int $num1=20;
+		int num2$=20;
+		System.out.println($);
+		System.out.println($num1+num2$);
+		
+		//out: 10
+		//     40
+	}
+
+	private static void VI() {
+		System.out.println('a'+'b');
+		//out: 195
+	}
+
+	private static void V() {
+		int i='1';
+		System.out.println(i);
+		//out: 1
+	}
+	private static void IV() {
+		int a=063;
+		System.out.println(a);
+		
+		//out: 063;
+	}
+	private static void III() {
+		
+		byte a=10;
+		(a+b byte b=10;
+		byte c=a+b;
+		System.out.println c);
+	}
+	//out: c.e
+	private static void II() {
+		int a;
+		int b=0;
+		static int c=0;
+		System.out.println();
+	}
+	//out:c.e
+	private static void I() {
+		
+			int a=10;
+			int b=20;
+			System.out.println("Sum :"+a+b);
+		
+	}
+//out: Sum :1020
+
+}
